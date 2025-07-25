4bf08f305863d107e686ddad82944a6c6d793fd7
DashSami <saman.karimian12@gmail.com>
2025-07-25 21:30:10 +0330

    first deploy on dashboard

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pB7akJJ/nEHaZDlju7JFcndWUgZTNup892sdMSyoasc=\"\n}"
--- shop/src/lib/format.js
+++
@@ -1,5 +0,0 @@
-export function formatPrice(price) {
-  // Convert Rial to Toman, format with commas, and append 'تومان'
-  const toman = Math.round(price / 1);
-  return toman.toLocaleString('fa-IR') + ' تومان';
-}
